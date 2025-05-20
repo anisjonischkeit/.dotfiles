37cdc98a7dc6f1ec4b7e6186d2a489e5fc767e62
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2025-05-20 23:00:42 +1000

    fixes

--- server/install.sh
+++ server/install.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-export STOW_FOLDERS="zsh"
-export OPTIONAL_STOW_FOLDERS="nvim"
+export STOW_FOLDERS="zsh,nvim"
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
-	echo "stow server/$folder"
-	stow -t ../../ -R $folder
-done
-
-for folder in $(echo $OPTIONAL_STOW_FOLDERS | sed "s/,/ /g"); do
-	pushd "../optional/"
-	echo "stow optional/$folder"
-	stow -t ../../ -R $folder
-	popd
+  echo "stow server/$folder"
+  stow -t ../../ -R $folder
 done
---
+++ server/nvim/.config/nvim/lua/plugins/zk-plus.lua
@@ -0,0 +1,6 @@
+return {
+	url = "git@github.com:anisjonischkeit/zk-plus.nvim.git",
+	config = function()
+		require("zk-plus").setup()
+	end,
+}

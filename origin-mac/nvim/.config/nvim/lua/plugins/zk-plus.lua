4c2ab61b4dc9aeb9704e4f6da31969eb2fff8dcd
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2025-05-24 20:32:11 +1000

    only add work/personal zow/p split for work laptop

--- origin-mac/nvim/.config/nvim/lua/plugins/zk-plus.lua
+++ origin-mac/nvim/.config/nvim/lua/plugins/zk-plus.lua
@@ -1,6 +1,20 @@
 return {
-  dir = "~/personal/zk-plus.nvim",
-  config = function()
-    require("zk-plus").setup()
-  end,
+	dir = "~/personal/zk-plus.nvim",
+	config = function()
+		require("zk-plus").setup()
+
+		-- Open notes.
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>zow",
+			"<Cmd>ZkNotes { sort = { 'modified' } , excludeHrefs = { './Personal' } }<CR>",
+			{ noremap = true, silent = false, desc = "Open Work Note" }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>zop",
+			"<Cmd>ZkNotes { sort = { 'modified' }, excludeHrefs = { './Organisations' } }<CR>",
+			{ noremap = true, silent = false, desc = "Open Personal Note" }
+		)
+	end,
 }
